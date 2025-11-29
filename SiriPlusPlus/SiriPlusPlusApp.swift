f26d4263af7748e3b8450551c7968e6327cc7a64
VedPanse <panse.ved@yandex.com>
2025-11-28 23:27:07 -0800

    Initial Commit

---
+++ SiriPlusPlus/SiriPlusPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriPlusPlusApp.swift
+//  SiriPlusPlus
+//
+//  Created by Ved Panse on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SiriPlusPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
